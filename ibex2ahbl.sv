80accf4aa6c22d4cbcf0ca823144c3fb35228006
ed-schwarz <81160530+ed-schwarz@users.noreply.github.com>
2024-07-03 16:41:14 +0200

    Update ibex2ahbl.sv

--- ibex2ahbl.sv
+++ ibex2ahbl.sv
@@ -53,11 +53,18 @@ module ibex2ahbl_bridge #(
 );
 logic drvalid_next;
 logic drvalid;
+logic [WIDTH-1:0] wdata_reg;
+logic [2:0]  dmem_size;
+logic [31:0]  data_wdata_temp;
+logic [31:0]  data_addr_temp;
+logic irvalid_next;
+logic irvalid;
 
 
 always @(posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
 		drvalid <= 1'b0;
+		wdata_reg <= '0;
 		
 	end else begin
 		if(drvalid && !dmem_hready) begin
@@ -65,11 +72,11 @@ always @(posedge clk or negedge rst_n) begin
 		end else begin
 			drvalid <= drvalid_next;
 		end
+		wdata_reg <= data_wdata_temp;
 	end
 	
 end
-logic irvalid_next;
-logic irvalid;
+
 
 
 always @(posedge clk or negedge rst_n) begin
@@ -92,26 +99,31 @@ always_comb begin
 	irvalid_next = instr_req & imem_hready;
 end
 
-logic [2:0]  dmem_size;
-logic [31:0]  data_wdata_temp;
-logic [31:0]  data_addr_temp;
+
 
 always_comb begin
 	data_wdata_temp = data_wdata;
 	data_addr_temp = data_addr;
-	case(data_be)
-		4'b0001: dmem_size = 3'd0;
-		4'b0011: dmem_size = 3'd1;
-		4'b1111: dmem_size = 3'd2;
-		4'b1100 : begin
-			dmem_size = 3'd1;
-			data_wdata_temp[15:0] = data_wdata[31:16];
-			data_addr_temp[31:2] = data_addr[31:2];
-			data_addr_temp[1:0] = 2'd2;
-		end
-		default : dmem_size = 3'd2;
-	endcase
+	if(!rst_n) begin
+		data_wdata_temp = '0;
+	end
+	else begin
+		case(data_be)
+			4'b0001: dmem_size = 3'd0;
+			4'b0011: dmem_size = 3'd1;
+			4'b1111: dmem_size = 3'd2;
+			4'b1100 : begin
+				dmem_size = 3'd1;
+				data_wdata_temp[15:0] = data_wdata[31:16];
+				data_addr_temp[31:2] = data_addr[31:2];
+				data_addr_temp[1:0] = 2'd2;
+			end
+			default : dmem_size = 3'd2;
+		endcase
+	end
 end
+
+
 
 assign imem_haddr = instr_addr;
 assign instr_rdata = imem_hrdata;
@@ -126,10 +138,10 @@ assign instr_rdata_intg = '0;
 assign imem_hsize = 3'd2;
 
 
-assign dmem_haddr = (^data_addr === 1'bx) ? '0 : data_addr_temp;
+assign dmem_haddr = data_addr_temp;
 assign data_rdata = dmem_hrdata;
 assign dmem_htrans = data_req ? 2'd2 : 2'd0;
-assign dmem_hwdata = (^data_wdata === 1'bx) ? '0 : data_wdata_temp;
+assign dmem_hwdata = wdata_reg;
 assign dmem_hprot = 4'b0011;
 assign dmem_hburst = '0;
 assign dmem_hmastlock = 0;
